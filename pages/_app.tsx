44ea4fd4c61714c8d54adb6c553de08a7911cb90
xerod <xerod@outlook.com>
2023-03-10 05:51:50 +0700

    fix: resolve unloaded themes on mounted

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,25 +36,25 @@ export default function App({ Component, pageProps }: AppProps) {
     document.body.removeAttribute('style');
 
     const theme = window.localStorage.getItem('theme') as ThemeType;
-    if (themes.includes(theme)) setThemeType(theme);
-  }, []);
+    if (themes.includes(theme)) switchTheme(theme);
+  }, [switchTheme]);
 
-  const myTheme1 = Themes.createFromDark({
-    type: 'coolTheme',
-    palette: {
-      background: "hsl(40, 54%, 2%, 100%)",
-      accents_1: "hsl(40, 54%, 4%, 100%)",
-      accents_2: "hsl(40, 54%, 60%, 20%)",
-      accents_3: "hsl(40, 54%, 60%, 50%)",
-      accents_4: "hsl(47, 54%, 50%)",
-      accents_5: "hsl(47, 54%, 60%)",
-      accents_6: "hsl(47, 54%, 70%)",
-      accents_7: "hsl(47, 54%, 80%)",
-      accents_8: "hsl(47, 54%, 85%)",
-      foreground: "hsl(47, 54%, 95%)",
-      border: "hsl(46, 54%, 11%, 100%)",
-    },
-  })
+  // const myTheme1 = Themes.createFromDark({
+  //   type: 'coolTheme',
+  //   palette: {
+  //     background: "hsl(40, 54%, 2%, 100%)",
+  //     accents_1: "hsl(40, 54%, 4%, 100%)",
+  //     accents_2: "hsl(40, 54%, 60%, 20%)",
+  //     accents_3: "hsl(40, 54%, 60%, 50%)",
+  //     accents_4: "hsl(47, 54%, 50%)",
+  //     accents_5: "hsl(47, 54%, 60%)",
+  //     accents_6: "hsl(47, 54%, 70%)",
+  //     accents_7: "hsl(47, 54%, 80%)",
+  //     accents_8: "hsl(47, 54%, 85%)",
+  //     foreground: "hsl(47, 54%, 95%)",
+  //     border: "hsl(46, 54%, 11%, 100%)",
+  //   },
+  // })
   return (
     // <GeistProvider themes={[myTheme1]} themeType={'coolTheme'}>
     <GeistProvider themeType={themeType}>
