e319673cc97046def967ff0f87919a2394c7eb7f
Ryan Tjhin <ryan_tjhin@hotmail.com>
2023-03-30 21:09:23 +0700

    added modal component on TokenPicker

--- components/modules/swap/TokenPicker.tsx
+++ components/modules/swap/TokenPicker.tsx
@@ -1,21 +1,37 @@
+import {
+  Modal,
+  ModalContents,
+  ModalOpenButton,
+} from "@/components/elements/Modal";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import React, { FC } from "react";
-import ETH from "../../public/logo/eth.svg";
 
 export const TokenPicker: FC = () => {
   return (
     <div className="left-0 right-0 mt-[-9px] mb-[-9px] flex items-center justify-center">
-      <button
-        // onClick={}
-        type="button"
-        className="flex items-center space-x-2 z-10 group bg-white hover:bg-white hover:dark:bg-[#2D3036]/50 dark:bg-[#2D3036] p-2 border-white transition-all rounded-lg cursor-pointer"
-      >
-        {/* <img src="/logo/eth.svg" /> */}
-        <span>ETH</span>
-        <div>
-          <ChevronDownIcon strokeWidth={3} className="w-4 h-4 " />
-        </div>
-      </button>
+      <Modal>
+        <ModalOpenButton>
+          <button
+            // onClick={<Modal />}
+            type="button"
+            className="flex items-center space-x-2 z-10 group bg-white hover:bg-white hover:dark:bg-[#2D3036]/50 dark:bg-[#2D3036] p-2 border-white transition-all rounded-lg cursor-pointer"
+          >
+            {/* <img src="/logo/eth.svg" /> */}
+            <span>ETH</span>
+            <div>
+              <ChevronDownIcon strokeWidth={3} className="w-4 h-4 " />
+            </div>
+          </button>
+        </ModalOpenButton>
+        <ModalContents>
+          {/* {({ close }) => <WalletGroupForm handleClose={close} />} */}
+          {({ close }) => (
+            <div>
+              <button onClick={() => close}></button>
+            </div>
+          )}
+        </ModalContents>
+      </Modal>
     </div>
   );
 };
