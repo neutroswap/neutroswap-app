9b62d5b0814abccfbb187b9e92ae4121f369ab43
xerod <xerod@outlook.com>
2023-04-06 17:17:41 +0700

    feat: add multicall contract to env

--- shared/helpers/contract.ts
+++ shared/helpers/contract.ts
@@ -3,3 +3,6 @@ export const ROUTER_CONTRACT = process.env.NEXT_PUBLIC_ROUTER_CONTRACT as `0x${s
 
 if (!process.env.NEXT_PUBLIC_FACTORY_CONTRACT) throw new Error("Missing env.NEXT_PUBLIC_FACTORY_CONTRACT");
 export const FACTORY_CONTRACT = process.env.NEXT_PUBLIC_FACTORY_CONTRACT as `0x${string}`
+
+if (!process.env.NEXT_PUBLIC_MULTICALL_CONTRACT) throw new Error("Missing env.NEXT_PUBLIC_MULTICALL_CONTRACT");
+export const MULTICALL_CONTRACT = process.env.NEXT_PUBLIC_MULTICALL_CONTRACT as `0x${string}`
