37d7bd6a34fd6d550ad7b6767de4c244ef8552e2
xerod <xerod@outlook.com>
2023-04-06 17:21:25 +0700

    feat: add pool deposit panel

--- pages/pool/[id].tsx
+++ pages/pool/[id].tsx
@@ -1,5 +1,5 @@
 import { classNames } from "@/shared/helpers/classNames";
-import { Text, Button, useTheme } from "@geist-ui/core";
+import { Text, Button, useTheme, Input } from "@geist-ui/core";
 import { Tab } from "@headlessui/react";
 import { ChevronLeftIcon } from "@heroicons/react/20/solid";
 import { ScaleIcon } from "@heroicons/react/24/solid";
@@ -8,8 +8,12 @@ import { useRouter } from "next/router";
 
 import NoContentDark from "@/public/states/empty/dark.svg"
 import NoContentLight from "@/public/states/empty/light.svg"
-import { useContractReads } from "wagmi";
-import { NEUTRO_POOL_ABI } from "@/shared/abi";
+import { useAccount, useContractRead, useContractReads } from "wagmi";
+import { NEUTRO_POOL_ABI, NEUTRO_ROUTER_ABI } from "@/shared/abi";
+import { UniswapPair, UniswapPairFactory, UniswapPairSettings, UniswapVersion } from "simple-uniswap-sdk";
+import { FACTORY_CONTRACT, MULTICALL_CONTRACT, ROUTER_CONTRACT } from "@/shared/helpers/contract";
+import { useEffect, useMemo, useState } from "react";
+import { formatEther, parseEther } from "ethers/lib/utils.js";
 
 export default function PoolDetails() {
   return (
@@ -51,7 +55,9 @@ export default function PoolDetails() {
             <Tab.Panel>
               <PoolOverviewPanel />
             </Tab.Panel>
-            <Tab.Panel>Content 2</Tab.Panel>
+            <Tab.Panel>
+              <PoolDepositPanel />
+            </Tab.Panel>
             <Tab.Panel>Content 3</Tab.Panel>
           </Tab.Panels>
         </div>
@@ -63,6 +69,76 @@ export default function PoolDetails() {
 const PoolOverviewPanel = () => {
   const router = useRouter();
   const theme = useTheme();
+  const { address } = useAccount();
+
+  // const [uniswapPairFactory, setUniswapPairFactory] = useState<UniswapPairFactory>();
+
+  // const poolContract = {
+  //   address: router.query.id as `0x${string}`,
+  //   abi: NEUTRO_POOL_ABI,
+  // }
+
+  // const { data, isError, isLoading } = useContractReads({
+  //   contracts: [
+  //     {
+  //       ...poolContract,
+  //       functionName: 'token0'
+  //     },
+  //     {
+  //       ...poolContract,
+  //       functionName: 'token1'
+  //     }
+  //   ]
+  // })
+
+  // let cloneUniswapContractDetailsV2 = useMemo(() => ({
+  //   routerAddress: ROUTER_CONTRACT,
+  //   factoryAddress: FACTORY_CONTRACT,
+  //   pairAddress: router.query.id as `0x${string}`,
+  // }), [router.query.id]);
+
+  // let customNetworkData = useMemo(() => ({
+  //   nameNetwork: "EOS EVM",
+  //   multicallContractAddress: MULTICALL_CONTRACT,
+  //   nativeCurrency: {
+  //     name: "EOS",
+  //     symbol: "EOS",
+  //   },
+  //   nativeWrappedTokenInfo: {
+  //     chainId: 15557,
+  //     contractAddress: "0x6cCC5AD199bF1C64b50f6E7DD530d71402402EB6",
+  //     decimals: 18,
+  //     symbol: "WEOS",
+  //     name: "Wrapped EOS",
+  //   },
+  // }), []);
+
+  // let customPairSettings = useMemo(() => new UniswapPairSettings({
+  //   slippage: 0.0005,
+  //   deadlineMinutes: 5,
+  //   disableMultihops: true,
+  //   cloneUniswapContractDetails: {
+  //     v2Override: cloneUniswapContractDetailsV2,
+  //   },
+  //   uniswapVersions: [UniswapVersion.v2],
+  //   customNetwork: customNetworkData,
+  // }), [cloneUniswapContractDetailsV2, customNetworkData])
+
+  // useEffect(() => {
+  //   if (!data) return;
+  //   (async () => {
+  //     const uniswapPair = new UniswapPair({
+  //       fromTokenContractAddress: data?.[0]!,
+  //       toTokenContractAddress: data?.[1]!,
+  //       ethereumAddress: address as string,
+  //       chainId: 15557,
+  //       providerUrl: "https://api-testnet2.trust.one/",
+  //       settings: customPairSettings,
+  //     })
+  //     const pairFactory = await uniswapPair.createFactory();
+  //     setUniswapPairFactory(pairFactory);
+  //   })()
+  // }, [data, address, customPairSettings])
 
   return (
     <div className="">
@@ -84,6 +160,61 @@ const PoolOverviewPanel = () => {
           {theme.type === "dark" && <NoContentDark className="w-40 h-40" />}
           <p className="text-neutral-500 text-center">You do not have any liquidity positions. Deposit some tokens to open a position.</p>
           <Button className="!mt-2">Deposit now</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+const PoolDepositPanel = () => {
+  const router = useRouter();
+  const theme = useTheme();
+  const { address } = useAccount();
+
+  const routerContract = {
+    address: ROUTER_CONTRACT,
+    abi: NEUTRO_ROUTER_ABI,
+  }
+
+  const poolContract = {
+    address: router.query.id as `0x${string}`,
+    abi: NEUTRO_POOL_ABI,
+  }
+
+  const { data: reserveData } = useContractRead({
+    ...poolContract,
+    functionName: 'getReserves'
+  });
+
+  // const { data } = useContractRead({
+  //   ...routerContract,
+  //   functionName: 'quote',
+  //   args: [parseEther("1"), reserveData?.[0]!, reserveData?.[1]!]
+  // });
+
+  const { data } = useContractRead({
+    ...poolContract,
+    functionName: 'name',
+    // args: [parseEther("1"), reserveData?.[0]!, reserveData?.[1]!]
+  });
+
+  return (
+    <div className="">
+      <div className="flex items-center space-x-3">
+        <ScaleIcon className="w-5 h-5 rounded-lg border border-neutral-300 dark:border-neutral-700 p-1.5" />
+        <p className="m-0 text-2xl font-semibold">Deposit</p>
+      </div>
+      <p className="mt-2 text-sm text-neutral-400 dark:text-neutral-600">Contract: {router.query.id}</p>
+      <p className="mt-2 text-sm text-neutral-400 dark:text-neutral-600">{reserveData ? formatEther(reserveData?.[0]) : 'error'}</p>
+      <p className="mt-2 text-sm text-neutral-400 dark:text-neutral-600">Name: {data}</p>
+
+      <div className="grid grid-cols-12">
+        <div className="w-full mt-4 border border-neutral-200/50 dark:border-neutral-800 rounded-lg col-span-8">
+          <div className="flex flex-col">
+            <Input className="w-full" />
+            <Button className="!mt-2">Deposit now</Button>
+          </div>
         </div>
       </div>
     </div>
