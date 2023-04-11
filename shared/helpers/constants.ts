415dbd006474081f2b9d3db69fdd625f19c70141
xerod <xerod@outlook.com>
2023-04-11 20:05:03 +0700

    fix: resolve error on missing SUPABASE_URL and SUPABASE_SERVICE_KEY

--- shared/helpers/constants.ts
+++ shared/helpers/constants.ts
@@ -1,2 +1,2 @@
 export const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
-export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+export const SUPABASE_URL = process.env.SUPABASE_URL;
--- shared/helpers/supabaseClient.ts
+++ shared/helpers/supabaseClient.ts
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 import { SUPABASE_URL, SUPABASE_SERVICE_KEY } from '@/shared/helpers/constants';
 
-if (!SUPABASE_URL) throw new Error("Missing env.NEXT_APP_SUPABASE_URL");
+if (!SUPABASE_URL) throw new Error("Missing env.SUPABASE_URL");
 if (!SUPABASE_SERVICE_KEY)
-  throw new Error("Missing env.NEXT_APP_SUPABASE_ANON_KEY");
+  throw new Error("Missing env.SUPABASE_SERVICE_KEY");
 
 export const supabaseClient = createClient(
   SUPABASE_URL,
