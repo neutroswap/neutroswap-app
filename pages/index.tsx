1f37870b7d4021fa277a09aaf2b6a5d4cda7cf7e
Ryan Tjhin <ryan_tjhin@hotmail.com>
2023-04-14 01:37:19 +0700

    Editted home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,14 @@
 
 import Navbar from "@/components/modules/Navbar";
 import { Button, Page, Text } from "@geist-ui/core";
-
+import Logo from "@/public/logo.svg";
+import Link from "next/link";
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <Text h1>Home Page</Text>
-      <Button>Submit</Button>
-    </>
+    <div className="flex justify-center items-center h-screen">
+      <Logo className="h-5 lg:h-40 text-black dark:text-white " />
+    </div>
   );
 }
