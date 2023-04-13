49be4848d7bf12a0948cfac82b95ac3659ae2489
Ryan Tjhin <ryan_tjhin@hotmail.com>
2023-04-14 01:04:32 +0700

    Changed navbar and added pages

---
+++ pages/vault/index.tsx
@@ -0,0 +1,17 @@
+// import { Inter } from 'next/font/google'
+
+import Navbar from "@/components/modules/Navbar";
+import { Button, Page, Text } from "@geist-ui/core";
+import Logo from "@/public/logo.svg";
+
+// const inter = Inter({ subsets: ['latin'] })
+
+export default function Vault() {
+  return (
+    <div className="flex flex-col h-screen justify-center items-center">
+      <Text h1 className="text-neutral-400">
+        Coming Soon
+      </Text>
+    </div>
+  );
+}
