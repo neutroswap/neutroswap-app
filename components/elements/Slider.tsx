1662ff713b98498d12ce6323ea6a62cebb7693e1
xerod <xerod@outlook.com>
2023-04-14 12:37:44 +0700

    style: restyling slider component

--- components/elements/Slider.tsx
+++ components/elements/Slider.tsx
@@ -12,8 +12,8 @@ import {
 
 const callAll =
   (...fns: any) =>
-  (...args: any) =>
-    fns.forEach((fn: any) => fn && fn(...args));
+    (...args: any) =>
+      fns.forEach((fn: any) => fn && fn(...args));
 
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
@@ -42,11 +42,12 @@ const Slider = React.forwardRef<
       )}
     >
       <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-neutral-200 dark:bg-neutral-900">
-        <SliderPrimitive.Range className="absolute h-full bg-neutral-400 dark:bg-neutral-700" />
+        <SliderPrimitive.Range className="absolute h-full bg-orange-500 dark:bg-orange-800" />
       </SliderPrimitive.Track>
       <span
         className={classNames(
-          "px-2 py-1 text-xs bg-neutral-200 dark:bg-neutral-800 rounded-lg",
+          "px-2 py-1 text-xs font-medium bg-neutral-100 dark:bg-neutral-800 rounded-lg",
+          "border border-neutral-200 dark:border-transparent",
           isHovered && "block animate-in slide-in-from-bottom-4 zoom-in-50",
           !isHovered && "hidden animate-out slide-out-to-bottom-4 zoom-out-50"
           // "-translate-x-[50%]"
@@ -64,7 +65,7 @@ const Slider = React.forwardRef<
         onMouseLeave={() => setIsHovered(false)}
         className={classNames(
           "block h-3 w-3 rounded-full hover:scale-125 transition-all",
-          "bg-neutral-400 dark:bg-neutral-500",
+          "bg-orange-800 dark:bg-orange-500",
           "disabled:pointer-events-none disabled:opacity-50",
           "border-1.5 border-neutral-400 dark:border-neutral-800",
           "focus:outline-none focus:ring-1 focus:ring-neutral-400 focus:ring-offset-2 dark:focus:ring-neutral-500 dark:focus:ring-offset-neutral-900"
