db96a9df8d5659e93a1e6565f5ca625ed4b50f9e
rwin <erwinphanglius10@gmail.com>
2023-04-20 22:18:48 +0700

    fix: find lpPrice to improve user info calculation

--- pages/api/getUserFarm.ts
+++ pages/api/getUserFarm.ts
@@ -32,6 +32,10 @@ interface Farm {
   lpToken: string,
   token0: string,
   token1: string,
+  token0decimals: number,
+  token1decimals: number,
+  token0denominator: number,
+  token1denominator: number,
   token0gecko: string,
   token1gecko: string,
   token0price: number,
@@ -39,7 +43,11 @@ interface Farm {
   reserves0: string,
   reserves1: string,
   reward: string,
+  valueReserves0: string,
+  valueReserves1: string,
   valueOfLiquidity: string,
+  circulatingSupply: string,
+  lpPrice: string,
   details: FarmDetails | null
 }
 
@@ -65,7 +73,7 @@ export async function getAllFarms(): Promise<Farm[] | null> {
 
   const { data, error } = await supabaseClient
     .from('farms')
-    .select('*,liquidity_tokens(address, token0(address,symbol,logo,coingecko_id), token1(address,symbol,logo,coingecko_id)),rewards:tokens(address)')
+    .select('*,liquidity_tokens(address, token0(address,symbol,logo,coingecko_id,decimal), token1(address,symbol,logo,coingecko_id,decimal)),rewards:tokens(address)')
     .eq('network_id', network[0].id)
   if (!data) { return null }
 
@@ -87,13 +95,21 @@ export async function getAllFarms(): Promise<Farm[] | null> {
         lpToken: data[i].liquidity_tokens.address,
         token0: data[i].liquidity_tokens.token0.address,
         token1: data[i].liquidity_tokens.token1.address,
+        token0decimals: data[i].liquidity_tokens.token0.decimal,
+        token1decimals: data[i].liquidity_tokens.token1.decimal,
+        token0denominator: Math.max(1, 18 - data[i].liquidity_tokens.token0.decimal),
+        token1denominator: Math.max(1, 18 - data[i].liquidity_tokens.token1.decimal),
         reward: data[i].rewards.address,
         token0gecko: data[i].liquidity_tokens.token0.coingecko_id,
         token1gecko: data[i].liquidity_tokens.token1.coingecko_id,
         token0price: 0,
         token1price: 0,
         reserves0: "",
+        valueReserves0: "",
+        valueReserves1: "",
         reserves1: "",
+        lpPrice: "",
+        circulatingSupply: "",
         valueOfLiquidity: "",
         details: null
       }
@@ -234,7 +250,7 @@ export async function totalValueOfLiquidity(farmHoldings: FarmHoldings) {
     reference: indexName + farm.lpToken,
     contractAddress: farm.lpToken,
     abi: [{ "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "spender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "sender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount0", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "amount1", "type": "uint256" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }], "name": "Burn", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "sender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount0", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "amount1", "type": "uint256" }], "name": "Mint", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "sender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount0In", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "amount1In", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "amount0Out", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "amount1Out", "type": "uint256" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }], "name": "Swap", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "internalType": "uint112", "name": "reserve0", "type": "uint112" }, { "indexed": false, "internalType": "uint112", "name": "reserve1", "type": "uint112" }], "name": "Sync", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "constant": true, "inputs": [], "name": "DOMAIN_SEPARATOR", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "MINIMUM_LIQUIDITY", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "PERMIT_TYPEHASH", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "internalType": "address", "name": "", "type": "address" }, { "internalType": "address", "name": "", "type": "address" }], "name": "allowance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "approve", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "internalType": "address", "name": "to", "type": "address" }], "name": "burn", "outputs": [{ "internalType": "uint256", "name": "amount0", "type": "uint256" }, { "internalType": "uint256", "name": "amount1", "type": "uint256" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "devFee", "outputs": [{ "internalType": "uint40", "name": "", "type": "uint40" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "factory", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "getReserves", "outputs": [{ "internalType": "uint112", "name": "_reserve0", "type": "uint112" }, { "internalType": "uint112", "name": "_reserve1", "type": "uint112" }, { "internalType": "uint32", "name": "_blockTimestampLast", "type": "uint32" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "internalType": "address", "name": "_token0", "type": "address" }, { "internalType": "address", "name": "_token1", "type": "address" }], "name": "initialize", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "kLast", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "internalType": "address", "name": "to", "type": "address" }], "name": "mint", "outputs": [{ "internalType": "uint256", "name": "liquidity", "type": "uint256" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "name", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "nonces", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }, { "internalType": "uint256", "name": "deadline", "type": "uint256" }, { "internalType": "uint8", "name": "v", "type": "uint8" }, { "internalType": "bytes32", "name": "r", "type": "bytes32" }, { "internalType": "bytes32", "name": "s", "type": "bytes32" }], "name": "permit", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "price0CumulativeLast", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "price1CumulativeLast", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "internalType": "address", "name": "to", "type": "address" }], "name": "skim", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "internalType": "uint256", "name": "amount0Out", "type": "uint256" }, { "internalType": "uint256", "name": "amount1Out", "type": "uint256" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "bytes", "name": "data", "type": "bytes" }], "name": "swap", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "swapFee", "outputs": [{ "internalType": "uint32", "name": "", "type": "uint32" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "sync", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "token0", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "token1", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "transfer", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }],
-    calls: [{ reference: 'reservesCall', methodName: 'getReserves', methodParameters: [] }]
+    calls: [{ reference: 'reservesCall', methodName: 'getReserves', methodParameters: [] }, { reference: 'totalSupplyCall', methodName: 'totalSupply', methodParameters: [] }]
   }));
 
   const contractCalls: ContractCallResults = await multicall.call(contractCallContext);
@@ -244,22 +260,39 @@ export async function totalValueOfLiquidity(farmHoldings: FarmHoldings) {
   for (const farm of farmHoldings.farms) {
     const reserves0 = contractCalls.results[indexName + farm.lpToken].callsReturnContext[0].returnValues[0]
     const reserves1 = contractCalls.results[indexName + farm.lpToken].callsReturnContext[0].returnValues[1]
+    const cirSupply = contractCalls.results[indexName + farm.lpToken].callsReturnContext[1].returnValues[0]
+    farm.circulatingSupply = formatEther(BigNumber.from(cirSupply.hex).toString())
 
     farm.reserves0 = BigNumber.from(reserves0.hex).toString()
     farm.reserves1 = BigNumber.from(reserves1.hex).toString()
 
     const reserve0 = BigNumber.from(farm.reserves0)
     const reserve1 = BigNumber.from(farm.reserves1)
-    const price0 = BigNumber.from(utils.parseUnits(farm.token0price.toString(), 18))
-    const price1 = BigNumber.from(utils.parseUnits(farm.token1price.toString(), 18))
-    const totalValueReserve0 = reserve0.mul(price0)
-    const totalValueReserve1 = reserve1.mul(price1)
-    const denominator = utils.parseUnits("1", 18);
-    farm.valueOfLiquidity = Number(formatEther(totalValueReserve0.add(totalValueReserve1).div(denominator).toString())).toFixed(2).toString()
+    const price0 = BigNumber.from(utils.parseUnits(farm.token0price.toString(), farm.token0decimals))
+    const price1 = BigNumber.from(utils.parseUnits(farm.token1price.toString(), farm.token1decimals))
+    // const denominator = utils.parseUnits("1", 18);
+    const denominator0 = utils.parseUnits("1", farm.token0decimals)
+    const denominator1 = utils.parseUnits("1", farm.token1decimals)
+
+    if (farm.token0decimals == 18) {
+      farm.valueReserves0 = formatEther(reserve0.mul(price0).div(denominator0)).toString()
+    } else {
+      farm.valueReserves0 = formatEther(reserve0.mul(price0).div(denominator0).mul(utils.parseUnits("1", farm.token0denominator))).toString()
+    }
+
+    if (farm.token1decimals == 18) {
+      farm.valueReserves1 = formatEther(reserve1.mul(price1).div(denominator1)).toString()
+    } else {
+      farm.valueReserves1 = formatEther(reserve1.mul(price1).div(denominator1).mul(utils.parseUnits("1", farm.token1denominator))).toString()
+    }
+    // farm.valueOfLiquidity = Number(formatEther(totalValueReserve0.add(totalValueReserve1).div(denominator).toString())).toFixed(2).toString()
+    farm.valueOfLiquidity = (parseFloat(farm.valueReserves0.toString()) + parseFloat(farm.valueReserves1.toString())).toFixed(2).toString()
+    farm.lpPrice = (parseFloat(farm.valueOfLiquidity.toString()) / parseFloat(farm.circulatingSupply.toString())).toFixed(2).toString()
+
     if (!farm.details?.totalStaked) { throw Error }
     farm.details = {
       totalStaked: farm.details.totalStaked,
-      totalStakedInUsd: (parseFloat(farm.details.totalStaked) * parseFloat(farm.valueOfLiquidity)).toString(),
+      totalStakedInUsd: (parseFloat(farm.details.totalStaked) * parseFloat(farm.lpPrice)).toString(),
       pendingTokens: farm.details.pendingTokens,
       pendingTokensInUsd: (parseFloat(farm.details.pendingTokens) * parseFloat("0.01")).toFixed(2).toString()
     }
