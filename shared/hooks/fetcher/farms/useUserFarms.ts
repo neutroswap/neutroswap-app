f516634f5c687908935f78bcf5f52eb9b579ed34
xerod <xerod@outlook.com>
2023-04-28 02:26:06 +0700

    fix: implement filter owned farm on useUserFarm

--- shared/hooks/fetcher/farms/useUserFarms.ts
+++ shared/hooks/fetcher/farms/useUserFarms.ts
@@ -21,8 +21,11 @@ const fetcher = (
   input: RequestInfo | URL,
   init?: RequestInit | undefined
 ) => fetch(input, init).then(async (res) => {
-  const result = await res.json();
-  return result.data
+  const { data }: { data: GetUserFarmsResponse } = await res.json();
+  const filteredFarm = data.farms.filter((farm) => {
+    return +farm.details.totalStaked > 0
+  })
+  return { ...data, farms: filteredFarm }
 })
 
 export default function useUserFarms(address?: `0x${string}`) {
