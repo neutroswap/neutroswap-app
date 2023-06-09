228dd6e8e4f997e56abc59a478ed3c1e1cdbd5cd
nayrr <nayrr@nayrrs-MacBook-Air.local>
2023-06-09 16:42:17 +0700

    fix: border currentEpoch

--- pages/dividend/index.tsx
+++ pages/dividend/index.tsx
@@ -60,14 +60,44 @@ export default function Dividend() {
         <div className="col-span-7 w-full mt-8">
           <div className="flex flex-col rounded">
             <div className="border border-neutral-200 dark:border-neutral-800/50 md:shadow-dark-sm md:dark:shadow-dark-lg">
-              <div className="flex flex-col justify-between items-start md:p-8">
+              <div className="flex flex-row justify-between items-start md:p-8 -mb-7">
                 <p className="m-0 text-left font-semibold whitespace-nowrap">
                   Current epoch
                 </p>
-                <span className="text-xs text-neutral-500">
-                  Min. estimated value
-                </span>
+                <div className="flex space-x-4">
+                  <button className="border px-3 py-1 text-xs text-neutral-500 font-semibold">
+                    Calculator
+                  </button>
+                </div>
               </div>
+              <div className="flex flex-col md:pl-8 m-0">
+                <div className="flex">
+                  <div className="w-1/2 flex items-center">
+                    <div className="flex">
+                      <EthLogo className="w-7 h-7 rounded-full" />
+                      <NeutroLogo className="w-7 h-7 rounded-full" />
+                    </div>
+                    <div className="ml-2">
+                      <span className="text-xs text-neutral-500">
+                        ETH-USDC.e
+                      </span>
+                      <div className="mt-0 text-xs text-neutral-400">0</div>
+                    </div>
+                  </div>
+
+                  <div className="w-1/2 flex items-center">
+                    <div className="flex">
+                      <EthLogo className="w-7 h-7 rounded-full" />
+                    </div>
+                    <div className="ml-2">
+                      <span className="text-xs text-neutral-500">xGRAIL</span>
+                      <div className="mt-0 text-xs text-neutral-400">0</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="grid grid-col-2"></div>
               <hr className="my-4 ml-8 w-11/12 border-neutral-200/80 dark:border-neutral-800/80" />
               <div className="flex flex-col justify-between items-start md:p-8 md:pt-0">
                 <div className="flex flex-col">
@@ -131,7 +161,7 @@ export default function Dividend() {
           </div>
 
           <hr className="my-7 border-neutral-200/80 dark:border-neutral-800/80" />
-          <div>
+          <div className="-space-y-12">
             <div className="flex flex-row items-center justify-between w-full md:p-8 md:pt-0">
               <p className="m-0 text-left font-semibold whitespace-nowrap">
                 Your dividends
@@ -142,17 +172,36 @@ export default function Dividend() {
                 </button>
               </div>
             </div>
-            <div className="flex flex-row items-center w-full md:p-8 md:mt-0 space-x-4">
-              <div className="flex">
-                <EthLogo className="w-7 h-7 rounded-full" />
-                <NeutroLogo className="w-7 h-7 rounded-full" />
+            <div className="flex flex-row items-center justify-between w-full md:p-8 md:mt-0">
+              <div className="flex items-center">
+                <div className="flex">
+                  <EthLogo className="w-7 h-7 rounded-full" />
+                  <NeutroLogo className="w-7 h-7 rounded-full" />
+                </div>
+                <div className="ml-2">
+                  <span className="text-xs text-neutral-500">ETH-USDC.e</span>
+                  <br />
+                  <span className="text-xs text-neutral-500">0</span>
+                </div>
               </div>
               <div>
-                <span className="text-xs text-neutral-500">ETH-USDC.e</span>
-                <br />
-                <span className="text-xs text-neutral-500">0</span>
+                <button className="px-5 py-2 border bg-grey-500 text-xs font-semibold rounded">
+                  Claim
+                </button>
               </div>
-              <div className="flex">
+            </div>
+
+            <div className="flex flex-row items-center justify-between w-full md:p-8 md:mt-0">
+              <div className="flex items-center">
+                <EthLogo className="w-7 h-7 rounded-full" />
+                <div className="w-7 h-7"></div>
+                <div className="ml-2">
+                  <span className="text-xs text-neutral-500">xGRAIL</span>
+                  <br />
+                  <span className="text-xs text-neutral-500">0</span>
+                </div>
+              </div>
+              <div>
                 <button className="px-5 py-2 border bg-grey-500 text-xs font-semibold rounded">
                   Claim
                 </button>
