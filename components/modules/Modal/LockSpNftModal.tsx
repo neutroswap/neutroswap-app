a257ba6a889543c89ba75eca71481a8b029b0e98
Ryan Tjhin <ryan_tjhin@hotmail.com>
2023-08-22 16:31:32 +0700

    feat: added slider for lock duration

--- components/modules/Modal/LockSpNftModal.tsx
+++ components/modules/Modal/LockSpNftModal.tsx
@@ -6,10 +6,12 @@ import {
   ModalContents,
   ModalOpenButton,
 } from "@/components/elements/Modal";
-import { LockClosedIcon, XCircleIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { ChevronDownIcon, LockClosedIcon } from "@heroicons/react/24/outline";
+import { XCircleIcon } from "@heroicons/react/24/solid";
+import { useMemo, useState } from "react";
 import { useAccount, useNetwork } from "wagmi";
+import * as Collapsible from "@radix-ui/react-collapsible";
+import { Slider } from "@/components/elements/Slider";
 
 export default function LockSpNftModal() {
   const { address } = useAccount();
@@ -17,8 +19,15 @@ export default function LockSpNftModal() {
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [open, setOpen] = useState<boolean>(false);
+  const [lockDuration, setLockDuration] = useState(0);
 
-  const form = useForm();
+  const handleSliderChange = (event: number[]) => {
+    setLockDuration(Number(event));
+  };
+
+  const handleSetMaxBonus = () => {
+    setLockDuration(180);
+  };
 
   const onSubmit = async () => {
     setIsLoading(true);
@@ -42,7 +51,7 @@ export default function LockSpNftModal() {
 
       <ModalContents>
         {({ close }) => (
-          <Form {...form}>
+          <>
             {/* <form onSubmit={form.handleSubmit(onSubmit)}> */}
             <div className="box-border relative">
               <XCircleIcon
@@ -74,11 +83,84 @@ export default function LockSpNftModal() {
                 <div className="px-2 py-1">Settings</div>
               </div>
 
-              <div className="flex justify-end text-xs text-neutral-500 mt-2">
+              <div className="flex px-2 mt-2 text-sm justify-between">
                 <div>
-                  <span>0%</span>
+                  Lock Duration:{" "}
+                  <span className="text-amber-500">{lockDuration}</span> Days
+                </div>
+                <div
+                  className="cursor-pointer text-amber-500"
+                  onClick={handleSetMaxBonus}
+                >
+                  Set max bonus
+                </div>
+              </div>
+
+              <div className="mt-5 px-2 ">
+                <Slider
+                  defaultValue={[0]}
+                  value={[lockDuration]}
+                  onValueChange={(e) => handleSliderChange(e)}
+                  max={180}
+                  step={1}
+                />
+              </div>
+
+              <div className="flex justify-end text-xs text-neutral-500 mt-2 px-2">
+                <div>
+                  <span>0% &nbsp;</span>
                   <span>Lock Bonus</span>
                 </div>
+              </div>
+
+              <div className="mt-4 font-medium bg-neutral-800 rounded-sm">
+                <div className="px-2 py-1">Estimates</div>
+              </div>
+
+              <div className="flex flex-col px-2 mt-2 text-sm">
+                <div className="w-full flex justify-between">
+                  <div>Deposit Value</div>
+                  <div className="text-neutral-500">
+                    $0.2 -&gt; <span className="text-white">$0.21</span>
+                  </div>
+                </div>
+
+                <Collapsible.Root
+                  className="w-full"
+                  open={open}
+                  onOpenChange={setOpen}
+                >
+                  <Collapsible.Trigger asChild>
+                    <div className="w-full flex justify-between hover:cursor-pointer hover:bg-neutral-900 py-2 rounded-sm ">
+                      <div className="flex">
+                        <div className="underline">Total APR</div>
+                        <ChevronDownIcon className="h-5 ml-1" />
+                      </div>
+                      <div className="text-neutral-500">
+                        21.96% -&gt;{" "}
+                        <span className="text-amber-500">30.15%</span>
+                      </div>
+                    </div>
+                  </Collapsible.Trigger>
+
+                  <Collapsible.Content className="py-2 flex flex-col gap-2 p-2">
+                    <div className="w-full flex justify-between">
+                      <div>Swap Fees APR</div>
+                      <div className="text-black dark:text-white">13.76%</div>
+                    </div>
+                    <div className="w-full flex justify-between">
+                      <div>Swap Base APR</div>
+                      <div className="text-black dark:text-white">8.2%</div>
+                    </div>
+                    <div className="w-full flex justify-between">
+                      <div>Farm Bonus APR</div>
+                      <div className="text-neutral-500">
+                        0% -&gt;{" "}
+                        <span className="text-black dark:text-white">0%</span>
+                      </div>
+                    </div>
+                  </Collapsible.Content>
+                </Collapsible.Root>
               </div>
 
               <div className="flex gap-2 mt-4">
@@ -114,7 +196,7 @@ export default function LockSpNftModal() {
               </div>
             </div>
             {/* </form> */}
-          </Form>
+          </>
         )}
       </ModalContents>
     </Modal>
