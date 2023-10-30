932aa6e868d8d260c71ca25770c8412612f91203
keivnjs <kjsurtan@gmail.com>
2023-10-30 23:14:15 +0700

    fix: refactor code

--- pages/dividend/index.tsx
+++ pages/dividend/index.tsx
@@ -1,36 +1,20 @@
-// import { Inter } from 'next/font/google'
-
 import Navbar from "@/components/modules/Navbar";
-import { Button, Page, Text } from "@geist-ui/core";
 import EthLogo from "@/public/logo/eth.svg";
 import NeutroLogo from "@/public/logo/neutro_token.svg";
 import EpochLogo from "@/public/logo/epoch.svg";
 import DeallocationLogo from "@/public/logo/deallocation.svg";
 import APYLogo from "@/public/logo/apy.svg";
 import AllocationLogo from "@/public/logo/allocation.svg";
-import AllocateDividendModal from "@/components/modules/Modal/AllocateDividendModal";
-import DeallocateDividendModal from "@/components/modules/Modal/DeallocateDividendModal";
-import {
-  useAccount,
-  useContractRead,
-  useContractReads,
-  useContractWrite,
-  usePrepareContractWrite,
-} from "wagmi";
+import { useContractReads } from "wagmi";
 import {
   NEXT_PUBLIC_DIVIDENDS_CONTRACT,
   NEXT_PUBLIC_NEUTRO_HELPER_CONTRACT,
-  NEXT_PUBLIC_NEUTRO_TOKEN_CONTRACT,
 } from "@/shared/helpers/constants";
 import { DIVIDENDS_ABI, NEUTRO_HELPER_ABI } from "@/shared/abi";
-import { useState } from "react";
 import { formatEther, formatUnits } from "ethers/lib/utils.js";
-import { waitForTransaction } from "@wagmi/core";
 import { currencyFormat } from "@/shared/helpers/currencyFormat";
 import Countdown from "@/components/modules/Countdown";
 import UserDividends from "./modules/UserDividends";
-import { BigNumber } from "ethers";
-// const inter = Inter({ subsets: ['latin'] })
 
 const masterData = {
   totalAllocation: 1000,
@@ -63,39 +47,8 @@ const masterData = {
 const currentEpochReward = masterData.currentEpochDetails;
 const nextEpochReward = masterData.nextEpochDetails;
 
-const allocationData = {
-  userTotalAllocation: 20.2,
-  manualAllocation: 12.2,
-  totalShare: 34.12,
-  redeemAllocation: 12.22,
-  // buat fetching ini jgn manual,
-  dividendTokens: [
-    {
-      tokenName: "ETH-USDC.e",
-      tokenAddress: 0x000000000000000,
-      logoToken0: "logoToken0",
-      logoToken1: "logoToken1",
-      pendingAmountInToken: 1.2,
-      pendingAmountInUsd: 200,
-    },
-    {
-      tokenName: "xNEUTRO",
-      tokenAddress: 0xfc43ba5d73afc7ae2745ea6c2f534b1f40871b34,
-      logoToken0: "logoToken0",
-      logoToken1: "logoToken1",
-      pendingAmountInToken: 0.00001,
-      pendingAmountInUsd: 12.82,
-    },
-  ],
-};
-
-const allocationReward = allocationData.dividendTokens;
-
 export default function Dividend() {
-  const { address } = useAccount();
-
   const { data } = useContractReads({
-    // enabled: Boolean(address),
     cacheOnBlock: true,
     allowFailure: false,
     contracts: [
@@ -132,30 +85,11 @@ export default function Dividend() {
     data?.[4][1]?.currentDistributionAmountInUsd
   );
 
-  const totalAllocation = data?.[0];
-
-  //Claim all button function
-  const { config: harvestAllConfig, refetch: refetchHarvestAllConfig } =
-    usePrepareContractWrite({
-      enabled: Boolean(address!),
-      address: NEXT_PUBLIC_DIVIDENDS_CONTRACT as `0x${string}`,
-      abi: DIVIDENDS_ABI,
-      functionName: "harvestAllDividends",
-    });
-  const { write: harvestAll, isLoading: isLoadingHarvestAll } =
-    useContractWrite({
-      ...harvestAllConfig,
-      onSuccess: async (tx) => {
-        await waitForTransaction({ hash: tx.hash });
-      },
-    });
+  const totalAllocation = formatEther(data?.[0] ?? 0);
+  const deallocationFee = formatEther(data?.[1] ?? 0);
 
   //countdown utils
-  const now = new Date();
-  let startTimeInterval = nextEpochReward.startTime * 1000 - now.getTime();
-  const targetEpochTime = Math.floor(
-    (now.getTime() + startTimeInterval) / 1000
-  );
+  const protocolEarningsTime = Math.floor(Number(data?.[2]));
 
   return (
     <div className="flex flex-col items-center sm:items-start justify-center sm:justify-between py-16">
@@ -181,7 +115,7 @@ export default function Dividend() {
                   </span>
                   <div className="flex space-x-1">
                     <span className="text-4xl md:text-3xl text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-yellow-500 font-semibold">
-                      {formatEther(totalAllocation ?? 0)}
+                      {totalAllocation}
                     </span>
                     <span className="text-sm text-neutral-500 mt-3">
                       xNEUTRO
@@ -233,7 +167,7 @@ export default function Dividend() {
                     Deallocation Fee
                   </span>
                   <span className="text-4xl md:text-3xl text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-yellow-500 font-semibold">
-                    {formatEther(data?.[1] ?? 0)}%
+                    {deallocationFee}%
                   </span>
                 </div>
                 <DeallocationLogo className="w-7 h-7 text-amber-500 rounded-full mt-3" />
@@ -310,7 +244,7 @@ export default function Dividend() {
                       <span className="text-sm text-neutral-500">
                         Remaining time
                       </span>
-                      <Countdown targetEpochTime={targetEpochTime} />
+                      <Countdown targetEpochTime={protocolEarningsTime} />
                     </div>
                   </div>
                 </div>
@@ -318,134 +252,8 @@ export default function Dividend() {
             </div>
           </div>
         </div>
-
-        {/* <div className="col-span-5 mt-8 flex flex-col rounded border border-neutral-200 dark:border-neutral-800/50 md:shadow-dark-sm dark:shadow-dark-lg">
-          <div>
-            <div className="flex flex-row items-center w-full md:p-8 justify-between">
-              <p className="m-0 text-left font-semibold whitespace-nowrap">
-                Your allocation
-              </p>
-              <div className="flex space-x-4">
-                <DeallocateDividendModal />
-                <AllocateDividendModal />
-              </div>
-            </div>
-            <div className="flex flex-col md:pl-8 m-0">
-              <div className="grid grid-cols-2 gap-2">
-                <div>
-                  <span className="text-sm text-neutral-500">
-                    Total Allocation
-                  </span>
-                  <div className="mt-1 text-sm">
-                    {allocationData.userTotalAllocation} xNEUTRO
-                  </div>
-                </div>
-                <div>
-                  <span className="text-sm text-neutral-500">Total Share</span>
-                  <div className="mt-1 text-sm">
-                    {allocationData.totalShare}%
-                  </div>
-                </div>
-                <div>
-                  <span className="text-sm text-neutral-500">
-                    Manual Allocation
-                  </span>
-                  <div className="mt-1 text-sm">
-                    {allocationData.manualAllocation} xNEUTRO
-                  </div>
-                </div>
-                <div>
-                  <span className="text-sm text-neutral-500">
-                    Redeem Allocation
-                  </span>
-                  <div className="mt-1 text-sm">
-                    {allocationData.redeemAllocation} xNEUTRO
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <hr className="my-7 border-neutral-200/80 dark:border-neutral-800/80" />
-          <div className="-space-y-12">
-            <div className="flex flex-row items-center justify-between w-full md:p-8 md:pt-0">
-              <p className="m-0 text-left font-semibold whitespace-nowrap">
-                Your dividends
-              </p>
-              <div className="flex space-x-4">
-                <Button
-                  className="px-4 py-2 border bg-amber-500 border-orange-600/50 text-xs font-semibold hover:bg-amber-600 rounded"
-                  onClick={() => harvestAll?.()}
-                  disabled={!harvestAll}
-                >
-                  Claim all
-                </Button>
-              </div>
-            </div>
-
-            {allocationReward.map((item, index) => (
-              <AllocationReward key={index} data={item} />
-            ))}
-          </div>
-        </div> */}
         <UserDividends />
       </div>
     </div>
   );
 }
-
-const AllocationReward = ({ data }: { data: any }) => {
-  const { address } = useAccount();
-
-  //Claim individual reward button function
-  const { config: harvestConfig, refetch: refetchHarvestConfig } =
-    usePrepareContractWrite({
-      enabled: Boolean(address!),
-      address: NEXT_PUBLIC_DIVIDENDS_CONTRACT as `0x${string}`,
-      abi: DIVIDENDS_ABI,
-      functionName: "harvestDividends",
-      args: [data.tokenAddress as `0x${string}`],
-    });
-  const { write: harvest, isLoading: isLoadingHarvest } = useContractWrite({
-    ...harvestConfig,
-    onSuccess: async (tx) => {
-      await waitForTransaction({ hash: tx.hash });
-    },
-  });
-
-  return (
-    <div className="flex flex-row items-center justify-between w-full md:p-8 md:mt-0">
-      <div className="flex items-center">
-        <div className="flex">
-          <EthLogo className="relative w-8 h-8 rounded-full" />
-          <NeutroLogo className="w-8 h-8 rounded-full -ml-2" />
-          {/* <img
-            src={data.logoToken0}
-            className="relative w-8 h-8 rounded-full"
-          />
-          <img src={data.logoToken1} className="w-8 h-8 rounded-full -ml-2" /> */}
-        </div>
-        <div className="ml-2">
-          <span className="text-sm text-neutral-500">{data.tokenName}</span>
-          <br />
-          <span className="text-sm">
-            {Number(data.pendingAmountInToken)} &nbsp;
-            <span className="text-neutral-500 text-xs">
-              ($
-              {currencyFormat(Number(data.pendingAmountInUsd))})
-            </span>
-          </span>
-        </div>
-      </div>
-      <div>
-        <Button
-          onClick={() => harvest?.()}
-          disabled={!harvest}
-          className="px-5 py-2 border bg-grey-500 text-xs font-semibold rounded"
-        >
-          Claim
-        </Button>
-      </div>
-    </div>
-  );
-};
