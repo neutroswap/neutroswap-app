6f43fb12c86d0b3ef031d09d0a6898813b9c37a5
keivnjs <kjsurtan@gmail.com>
2023-10-31 17:15:41 +0700

    fix: add factory & launchpad graph url

--- shared/config/urls.ts
+++ shared/config/urls.ts
@@ -2,15 +2,23 @@ import { ChainId } from "eth-chains";
 import { SupportedChainID } from "../types/chain.types";
 
 type Urls = {
+  FACTORY_GRAPH_URL: string;
+  LAUNCHPAD_GRAPH_URL: string;
   NFT_GRAPH_URL: string;
 };
 
 export const urls: Record<SupportedChainID, Urls> = {
   "17777": {
+    FACTORY_GRAPH_URL: "https://mainnet.esper-gn.com/subgraphs/name/esper-amm",
+    LAUNCHPAD_GRAPH_URL:
+      "https://api.thegraph.com/subgraphs/name/erwinphanglius/galahad-indexer",
     NFT_GRAPH_URL: "http://13.59.70.85:8000/subgraphs/name/neutroswap-nitro",
   },
 
   "15557": {
+    FACTORY_GRAPH_URL: "https://testnet.esper-gn.com/subgraphs/name/esper-amm",
+    LAUNCHPAD_GRAPH_URL:
+      "https://testnet.esper-gn.com/subgraphs/name/esper-launchpad",
     NFT_GRAPH_URL: "http://13.59.70.85:8000/subgraphs/name/neutroswap-nitro",
   },
 };
