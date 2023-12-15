a20c8a5299f6070b107cd526a70de272d06ab53c
keivnjs <kjsurtan@gmail.com>
2023-12-15 19:06:08 +0700

    fix: update NFT_GRAPH_URL

--- shared/config/urls.ts
+++ shared/config/urls.ts
@@ -12,13 +12,13 @@ export const urls: Record<SupportedChainID, Urls> = {
     FACTORY_GRAPH_URL: "https://mainnet.esper-gn.com/subgraphs/name/esper-amm",
     LAUNCHPAD_GRAPH_URL:
       "https://api.thegraph.com/subgraphs/name/erwinphanglius/galahad-indexer",
-    NFT_GRAPH_URL: "http://13.59.70.85:8000/subgraphs/name/neutroswap-nitro",
+    NFT_GRAPH_URL: "http://13.59.70.85:8000/subgraphs/name/neutro-nftPool",
   },
 
   "15557": {
     FACTORY_GRAPH_URL: "https://testnet.esper-gn.com/subgraphs/name/esper-amm",
     LAUNCHPAD_GRAPH_URL:
       "http://13.59.70.85:8000/subgraphs/name/neutro-launchpad",
-    NFT_GRAPH_URL: "http://13.59.70.85:8000/subgraphs/name/neutroswap-nitro",
+    NFT_GRAPH_URL: "http://13.59.70.85:8000/subgraphs/name/neutro-nftPool",
   },
 };
