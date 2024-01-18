b14fddce5f27c248360033819fbd0c703e9a6d72
keivnjs <kjsurtan@gmail.com>
2024-01-18 16:08:29 +0700

    fix: hide lock bonus countdown component if 0

--- components/modules/Modal/SpNftModal.tsx
+++ components/modules/Modal/SpNftModal.tsx
@@ -1,18 +1,10 @@
 "use client";
 
 import { cn, currencyFormat } from "@/shared/utils";
-import { NEUTRO_HELPER_ABI, NFT_POOL_ABI, XNEUTRO_ABI } from "@/shared/abi";
-import { NEXT_PUBLIC_NEUTRO_HELPER_CONTRACT } from "@/shared/helpers/constants";
-import { formatEther, parseEther } from "viem";
+import { NEUTRO_HELPER_ABI, NFT_POOL_ABI } from "@/shared/abi";
+import { formatEther } from "viem";
 import { useMemo, useState } from "react";
-import {
-  useAccount,
-  useContractRead,
-  useContractReads,
-  useContractWrite,
-  usePrepareContractWrite,
-  useNetwork,
-} from "wagmi";
+import { useAccount, useContractReads, useNetwork } from "wagmi";
 import {
   Lock as LockIcon,
   Lightning,
@@ -25,7 +17,6 @@ import {
   ArrowsLeftRight,
   Icon,
   CaretLeft,
-  Wallet,
 } from "@phosphor-icons/react";
 import * as Tabs from "@radix-ui/react-tabs";
 import AddToSpNftModal from "./AddToSpNftModal";
@@ -53,241 +44,6 @@ type TabItems = {
   hidden?: boolean;
   disabled?: boolean;
 }[];
-
-// export default function SpNftModal(props: Props) {
-//   const { address } = useAccount();
-//   const { chain } = useNetwork();
-
-//   const [isLoading, setIsLoading] = useState<boolean>(false);
-//   const [open, setOpen] = useState<boolean>(false);
-
-//   const form = useForm();
-//   const onSubmit = async () => {
-//     setIsLoading(true);
-//     if (!address) return new Error("Not connected");
-
-//     // if (!isApproved) {
-//     //   approve?.();
-//     // }
-
-//     // if (isApproved) {
-//     //   allocate?.();
-//   };
-
-//   return (
-//     <Modal>
-//       <ModalOpenButton>
-//         <button className="px-3 py-2 border rounded-md border-neutral-200 dark:border-neutral-800 mr-6 flex space-x-2">
-//           <span className="text-black dark:text-white text-sm font-semibold">
-//             Import token
-//           </span>
-//         </button>
-//       </ModalOpenButton>
-//       <ModalContents>
-//         {({ close }) => (
-//           <Form {...form}>
-//             <form onSubmit={form.handleSubmit(onSubmit)}>
-//               <div className="box-border">
-//                 <div className="flex items-center justify-between">
-//                   <div className="flex items-center justify-center w-full text-xl font-bold text-neutral-500">
-//                     Token Name
-//                   </div>
-//                   <XCircleIcon
-//                     onClick={() => close()}
-//                     className="h-6 cursor-pointer text-black dark:text-white opacity-50"
-//                   />
-//                 </div>
-
-//                 <div className="flex justify-center items-center mt-2 gap-2">
-//                   <span className="text-xl font-semibold">$0.1</span>
-//                   <span className="text-xl font-semibold"> &#x2014;</span>
-//                   <span className="text-amber-500 text-xl font-semibold">
-//                     9.27 %
-//                   </span>
-//                   <span className="text-xl font-semibold">APR</span>
-//                 </div>
-//                 <div className="flex justify-center">
-//                   <div className="text-neutral-500">
-//                     This position has{" "}
-//                     <span className="text-black dark:text-white">$0.01</span>{" "}
-//                     pending farming rewards
-//                   </div>
-//                 </div>
-//                 <div className="flex flex-col items-center justify-center">
-//                   <div className="flex flex-row justify-center space-x-7 m-5">
-//                     <div className="flex flex-col items-center">
-//                       <AddToSpNftModal />
-//                       <div className="text-xs text-amber-500 mt-2 text-center">
-//                         Add
-//                       </div>
-//                     </div>
-//                     <div className="flex flex-col items-center">
-//                       <WithdrawFromSpNftModal />
-//                       <div className="text-xs text-amber-500 mt-2 text-center">
-//                         Withdraw
-//                       </div>
-//                     </div>
-//                     <div className="flex flex-col items-center">
-//                       <LockSpNftModal />
-//                       <div className="text-xs text-amber-500 mt-2 text-center">
-//                         Lock
-//                       </div>
-//                     </div>
-//                     <div className="flex flex-col items-center">
-//                       <BoostSpNftModal />
-//                       <div className="text-xs text-amber-500 mt-2 text-center whitespace-nowrap">
-//                         Yield Boost
-//                       </div>
-//                     </div>
-//                     <div className="flex flex-col items-center">
-//                       <StakeNitroModal />
-//                       <div className="text-xs text-sky-500 mt-2 text-center">
-//                         Stake in Nitro
-//                       </div>
-//                     </div>
-//                   </div>
-//                 </div>
-
-//                 {/* <div className="flex justify-center mb-5">
-//                   <Collapsible.Root
-//                     className="w-[300px]"
-//                     open={open}
-//                     onOpenChange={setOpen}
-//                   >
-//                     <Collapsible.Trigger asChild>
-//                       <div className="flex justify-center items-center">
-//                         <button className="text-xs flex gap-2 text-neutral-500">
-//                           More actions
-//                           <ChevronDownIcon className="w-3 h-3 mt-1" />
-//                         </button>
-//                       </div>
-//                     </Collapsible.Trigger>
-
-//                     <Collapsible.Content>
-//                       <div className="flex flex-row justify-center space-x-7 m-5">
-//                         <div className="flex flex-col items-center">
-//                           <TransferPositionModal />
-//                           <div className="text-xs text-amber-500 mt-2 text-center">
-//                             Transfer
-//                           </div>
-//                         </div>
-//                         <div className="flex flex-col items-center">
-//                           <SplitPositionModal />
-//                           <div className="text-xs text-amber-500 mt-2 text-center">
-//                             Split
-//                           </div>
-//                         </div>
-//                         <div className="flex flex-col items-center">
-//                           <MergePositionModal />
-//                           <div className="text-xs text-amber-500 mt-2 text-center">
-//                             Merge
-//                           </div>
-//                         </div>
-//                       </div>
-//                     </Collapsible.Content>
-//                   </Collapsible.Root>
-//                 </div> */}
-
-//                 <div className="items-start flex justify-start">
-//                   <div className="w-full border-box bg-primary">
-//                     <span className="text-neutral-500">Properties</span>
-//                   </div>
-//                 </div>
-
-//                 <div className="flex flex-col space-y-3 mb-7">
-//                   <div>
-//                     <div className="flex justify-between items-center py-1 px-2">
-//                       <div className="flex flex-col items-start space-y-1">
-//                         <div className="flex items-center space-x-2">
-//                           <DeallocationLogo className="w-4 h-4" />
-//                           <div className="text-md text-neutral-500 mb-0">
-//                             Yield Booster
-//                           </div>
-//                         </div>
-//                       </div>
-//                       <div className="text-right">
-//                         <div className="text-sm font-semibold">10%</div>
-//                         <span className="text-xs text-neutral-500">
-//                           Farm APR
-//                         </span>
-//                       </div>
-//                     </div>
-//                   </div>
-
-//                   <div>
-//                     <div className="flex justify-between items-center py-1 px-2">
-//                       <div className="flex flex-col items-start space-y-1">
-//                         <div className="flex items-center space-x-2">
-//                           <DeallocationLogo className="w-4 h-4" />
-//                           <div className="text-md text-neutral-500 mb-0">
-//                             Unlocked
-//                           </div>
-//                         </div>
-//                       </div>
-//                       <div className="text-right">
-//                         <div className="text-sm font-semibold">-</div>
-//                         {/* <span className="text-xs text-neutral-500">Farm APR</span> */}
-//                       </div>
-//                     </div>
-//                   </div>
-
-//                   <div>
-//                     <div className="flex justify-between items-center py-1 px-2">
-//                       <div className="flex flex-col items-start space-y-1">
-//                         <div className="flex items-center space-x-2">
-//                           <DeallocationLogo className="w-4 h-4" />
-//                           <div>
-//                             <div className="text-md text-neutral-500 mb-0">
-//                               Boosted
-//                             </div>
-//                             <span className="text-xs text-neutral-500">
-//                               2x multiplier
-//                             </span>
-//                           </div>
-//                         </div>
-//                       </div>
-//                       <div className="text-right">
-//                         <div className="text-sm font-semibold">
-//                           0.00001 xNEUTRO
-//                         </div>
-//                         <span className="text-xs text-neutral-500">
-//                           Boost Allocation
-//                         </span>
-//                       </div>
-//                     </div>
-//                   </div>
-
-//                   <div>
-//                     <div className="flex justify-between items-center py-1 px-2">
-//                       <div className="flex flex-col items-start space-y-1">
-//                         <div className="flex items-center space-x-2">
-//                           <DeallocationLogo className="w-4 h-4" />
-//                           <div className="text-md text-neutral-500 mb-0">
-//                             Not staked in Nitro pools
-//                           </div>
-//                         </div>
-//                       </div>
-//                       <div className="text-right">
-//                         <div className="text-sm font-semibold">-</div>
-//                         {/* <span className="text-xs text-neutral-500">Farm APR</span> */}
-//                       </div>
-//                     </div>
-//                   </div>
-//                 </div>
-
-//                 <DataBreakdown />
-
-//                 <Button type="button" variant="outline" onClick={() => close()}>
-//                   Close
-//                 </Button>
-//               </div>
-//             </form>
-//           </Form>
-//         )}
-//       </ModalContents>
-//     </Modal>
-//   );
-// }
 
 export default function SPNFTSettingsModal(props: Props) {
   const { children } = props;
@@ -605,11 +361,12 @@ function Overview(props: GetNFTPositionResponse) {
                   {currencyFormat(item.apr, 2, 0.01)}% APR
                 </p>
               )}
-              {item.name === "Lock Bonus" && (
+              {item.name === "Lock Bonus" && lockDurationEpoch > now ? (
                 <p className="text-sm text-right">
                   <Countdown targetEpochTime={lockDurationEpoch} />
                 </p>
-              )}
+              ) : null}
+              {/* <pre>{JSON.stringify(props.endLockTime)}</pre> */}
             </div>
           </div>
         ))}
